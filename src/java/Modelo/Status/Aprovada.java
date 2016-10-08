18b0727cc0dfb6850bc2e93fbeb15852f927bfde
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2016-10-08 13:41:56 -0300

    Adicionando framework entitites

---
+++ src/java/Modelo/Status/Aprovada.java
@@ -0,0 +1,28 @@
+package Modelo.Status;
+
+public class Aprovada extends Status {
+
+    @Override
+    public void solicitar() {
+        throw new IllegalStateException("Não é possível solicitar, solicitação já APROVADA!");
+
+    }
+
+    @Override
+    public void aprovar() {
+        throw new IllegalStateException("Não é possível aprovar, solicitação já APROVADA!");
+
+    }
+
+    @Override
+    public void recusar() {
+        throw new IllegalStateException("Não é possível recusar, solicitação já APROVADA!");
+
+    }
+
+    @Override
+    public void retornar(String observacao) {
+        throw new IllegalStateException("Não é possível retornar, solicitação já APROVADA!");
+    }
+
+}
---
+++ src/java/Modelo/Status/NovaSolicitacao.java
@@ -0,0 +1,28 @@
+package Modelo.Status;
+
+public class NovaSolicitacao extends Status {
+
+    @Override
+    public void solicitar() {
+        System.out.println("Solicitação efetuada com sucesso.");
+        this.solicitacao.setStatus(new AguardandadoChefia());
+    }
+
+    @Override
+    public void aprovar() {
+        throw new IllegalStateException("Não é possivel aprovar, solicitação aguardando chefia");
+
+    }
+
+    @Override
+    public void recusar() {
+        throw new IllegalStateException("Não é possivel recusar, solicitação aguardando chefia");
+
+    }
+
+    @Override
+    public void retornar(String observacao) {
+        throw new IllegalStateException("Não é possivel retornar, solicitação aguardando chefia");
+    }
+
+}
